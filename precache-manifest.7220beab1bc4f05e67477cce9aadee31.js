2a77aeaa678e2822ae9fe66d88c9f86adbf002b3
mattjliu <matthew.liu041296@gmail.com>
2020-07-27 21:56:16 -0400

    Updates

---
+++ precache-manifest.7220beab1bc4f05e67477cce9aadee31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4aed50fcb0c92777cd7f121a4682fca7",
+    "url": "/Notation-Trainer/index.html"
+  },
+  {
+    "revision": "a04d74d1a9f35634e9f3",
+    "url": "/Notation-Trainer/static/css/main.0131da13.chunk.css"
+  },
+  {
+    "revision": "1332eeb39fa6f0fd52fa",
+    "url": "/Notation-Trainer/static/js/2.86be71ab.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Notation-Trainer/static/js/2.86be71ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a04d74d1a9f35634e9f3",
+    "url": "/Notation-Trainer/static/js/main.d6078859.chunk.js"
+  },
+  {
+    "revision": "e6137221193205c6a890",
+    "url": "/Notation-Trainer/static/js/runtime-main.a361197f.js"
+  }
+]);
